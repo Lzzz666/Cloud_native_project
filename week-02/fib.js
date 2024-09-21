9c6206145a0b99abc4905f059b11d66e7e74eaac
Kevin Liu <58145495+Lzzz666@users.noreply.github.com>
2024-09-21 17:20:40 +0800

    feat: 創建並完成 fib.js

--- week-02/fib.js
+++ week-02/fib.js
@@ -1,1 +1,16 @@
- 
+function fib(n){
+  if(n < 0){
+    return false;
+  }
+  if(n == 0){
+    return 0;
+  }
+  if(n == 1){
+    return 1;
+  }
+  return fib(n-1) + fib(n-2)
+}
+fib(0);
+fib(1);
+fib(5);
+fib(10);
