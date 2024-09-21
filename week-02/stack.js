7766cf88e48246fc14de0cda1782d5710a518801
Kevin Liu <58145495+Lzzz666@users.noreply.github.com>
2024-09-21 17:26:13 +0800

    feat: 創建並完成 stack.js

---
+++ week-02/stack.js
@@ -0,0 +1,48 @@
+// stack.js
+export default class Stack {
+	// TODO: # 為 class 內的私有屬性。無法被外部存取
+  #items;
+
+  constructor() {
+    this.#items = [];
+  }
+
+  // 在 stack 頂部加入元素i
+  push(element) {
+		this.#items.push(element);
+  }
+
+  // 移除並回傳 stack 頂部的元素
+  pop() {
+		return this.#items.pop();
+  }
+
+  // 回傳 stack 頂部的元素，但不移除它
+  peek() {
+    return this.#items[this.#items.length-1];
+  }
+
+  // 檢查 stack 是否為空
+  isEmpty() {
+    return (this.#items.length) == 0 ? True : False;
+  }
+
+  // 回傳 stack 中元素的個數
+  size() {
+    return this.#items.length;
+  }
+
+  // 清空 stack 
+  clear() {
+    while(this.#items.length != 0){
+        this.#items.pop();
+    }
+  }
+
+  // 印出 stack 內容
+  print() {
+    for(let i = this.#items.length-1;i >= 0;i--){
+        console.log(this.#items[i]);
+    }
+  }
+}
